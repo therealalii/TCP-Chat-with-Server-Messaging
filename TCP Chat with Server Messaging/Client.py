042ed8e55e32c87cdefb2af1ab46b501516260f5
Ali Rezaei <166546891+therealalii@users.noreply.github.com>
2025-08-11 14:18:14 +0330

    Update Client.py

--- TCP Chat with Server Messaging/Client.py
+++ TCP Chat with Server Messaging/Client.py
@@ -3,7 +3,6 @@ import threading
 
 SERVER_IP = input("Enter server IPv4 address: ").strip()
 PORT = 5555
-
 nickname = input("Choose your nickname: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,28 +11,23 @@ client.connect((SERVER_IP, PORT))
 def receive():
     while True:
         try:
-            message = client.recv(1024).decode("utf-8")
-            if message == "NICK":
-                client.send(nickname.encode("utf-8"))
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NICK':
+                client.send(nickname.encode('utf-8'))
             else:
                 print(message)
-        except Exception as e:
-            print(f"An error occurred: {e}")
+        except:
+            print("You have been disconnected from the server.")
             client.close()
             break
 
 def write():
     while True:
         try:
-            msg_text = input("")
-            message = f"{nickname}: {msg_text}"
-            client.send(message.encode("utf-8"))
-        except Exception as e:
-            print(f"Send error: {e}")
+            message = f"{nickname}: {input('')}"
+            client.send(message.encode('utf-8'))
+        except:
             break
 
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
-
-write_thread = threading.Thread(target=write)
-write_thread.start()
+threading.Thread(target=receive).start()
+threading.Thread(target=write).start()
